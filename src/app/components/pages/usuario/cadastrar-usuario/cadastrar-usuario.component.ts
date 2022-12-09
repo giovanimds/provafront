c58903b4b7198aca31537ed368bc733fddaa090b
giovani <giovanimoresco@gmail.com>
2022-12-08 21:59:42 -0300

    TERMINEI 2

--- src/app/components/pages/usuario/cadastrar-usuario/cadastrar-usuario.component.ts
+++ src/app/components/pages/usuario/cadastrar-usuario/cadastrar-usuario.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Usuario} from "../../../../Model/Usuario";
 import {Router} from "@angular/router";
@@ -8,15 +8,24 @@ import {Router} from "@angular/router";
   templateUrl: './cadastrar-usuario.component.html',
   styleUrls: ['./cadastrar-usuario.component.css']
 })
-export class CadastrarUsuarioComponent {
+export class CadastrarUsuarioComponent implements OnInit{
   public usuario!: Usuario;
+
+  ngOnInit() {
+    this.usuario = {
+      usuarioId: 0,
+      nome: "",
+      nascimento: Date.now().toString()
+    }
+  }
+
   constructor(private http:HttpClient, private router: Router) {
   }
   cadastrar(){
     this.http.post<Usuario>("https://localhost:5001/api/usuario/cadastrar", this.usuario).subscribe((usuario) => {
       alert("OK, CONSOLE.TABLE")
       console.table(usuario)
-      this.router.navigate(["cadastrar-imc"])
+      this.router.navigate([""])
     })
   }
 }
